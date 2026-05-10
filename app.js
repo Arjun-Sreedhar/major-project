a329eff8f2611d434add2ba4a0ba6c4e9bf5459a
Arjun Sreedhar <arjunsn03@gmail.com>
2026-05-10 14:14:56 +0530

    fix error page context

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.use((req, res, next) => {
 
 app.use((err,req,res,next)=>{
     let{statusCode=500 , message="Something went wrong"}=err; 
-    res.status(statusCode).render("listings/error.ejs",{message});
+    res.status(statusCode).render("listings/error.ejs",{message, currUser: req.user});
 });
 
 app.listen(port,()=>{
